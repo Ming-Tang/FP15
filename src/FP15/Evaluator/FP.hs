be245e9535e6d8b1270987c8da0700baf54d4eef
Ming-Tang <project.shinkirou@gmail.com>
2015-09-27 15:38:49 -0700

    Add RWST to FP monad.

--- src/FP15/Evaluator/FP.hs
+++ src/FP15/Evaluator/FP.hs
@@ -5,13 +5,17 @@
 {-# LANGUAGE StandaloneDeriving #-}
 module FP15.Evaluator.FP where
 import Control.Applicative
-import Control.Monad.RWS
+import Control.Monad.RWS.Strict
 import Control.Monad.Error
 import FP15.Evaluator.RuntimeError
 
+type R = ()
+type W = ()
+type S = ()
+
 -- | The 'FP' monad is for executing FP15 code. This monad contains 'IO' and
 -- error handling ('RuntimeError').
-newtype FP a = FP { unFP :: ErrorT RuntimeError IO a }
+newtype FP a = FP { unFP :: ErrorT RuntimeError (RWST R W S IO) a }
 
 -- IO (Error RuntimeError a)
 
@@ -30,5 +34,8 @@ instance Applicative FP where
 instance Functor FP where
   fmap f (FP x) = FP $ fmap f x
 
-runFP :: FP a -> IO (Either RuntimeError a)
-runFP = runErrorT . unFP
+runFP :: R -> S -> FP a -> IO (Either RuntimeError a, W, S)
+runFP r s = flip (`runRWST` r) s . runErrorT . unFP
+
+execFP :: FP a -> IO (Either RuntimeError a)
+execFP fp = (\(a, _, _) -> a) <$> runFP () () fp
