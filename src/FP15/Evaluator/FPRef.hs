467ecbad9af5e6626e3e355256361b68245ecb0a
Ming-Tang <project.shinkirou@gmail.com>
2015-12-01 09:56:30 -0800

    Add FPRef methods (WIP)

--- src/FP15/Evaluator/FPRef.hs
+++ src/FP15/Evaluator/FPRef.hs
@@ -1,5 +1,13 @@
 module FP15.Evaluator.FPRef where
 import Data.IORef
+import FP15.Evaluator.FP
 
 type Rev = (Int, Int)
 data FPRef a = FPRef Rev (IORef a)
+
+newFPRef :: a -> FP (FPRef a)
+readFPRef :: FPRef a -> FP a
+writeFPRef :: FPRef a -> FP ()
+modifyFPRef :: FPRef a -> (a -> a) -> FP ()
+modifyFPRef' :: FPRef a -> (a -> a) -> FP ()
+
